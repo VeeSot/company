3aa4ebd9c1ae9e12e8fb33a7d2f233875aea6401
Yozhkin Code <yozhkin.code@gamil.com>
2020-08-18 20:01:17 +0400

    Branch:New model

--- company/models.py
+++ company/models.py
@@ -1,12 +1,19 @@
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
 
 class Employee(AbstractUser):
     position_title = models.CharField(max_length=100,
                                       verbose_name="Position title")
+
     class Meta:
         verbose_name = "Employee"
         verbose_name_plural = "Employee"
 
 
+class Branch(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Branch name")
+    facade_image = models.ImageField(null=True,upload_to ='uploads/',
+                                     verbose_name="Facade image")
+    longitude = models.FloatField(db_index=True,verbose_name="Longitude")
+    latitude = models.FloatField(db_index=True,verbose_name="Latitude")
